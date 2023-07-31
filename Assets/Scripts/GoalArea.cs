afdf7a193aec3f77cf506cf1089acbbf97235cf1
chris-pitre <c.tophanp3@gmail.com>
2023-07-31 14:57:57 -0400

    started work on goal area
    added probuilder

---
+++ Assets/Scripts/GoalArea.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalArea : MonoBehaviour
+{
+    //todo: add references and stuff to dictate whenever a game object enters or something
+
+
+}
